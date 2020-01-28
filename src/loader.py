090f3347c0b9ef1228a7e19afbbcd92ee65c051f
mukesh-mehta <mukesh.iitr18@gmail.com>
2020-01-28 16:32:47 +0530

    updated loader code to read img from arr

--- src/loader.py
+++ src/loader.py
@@ -1,29 +1,23 @@
-import os
 import numpy as np
-from .utils import load_image
 from torch.utils import data
 
 
 class DataLoader(data.Dataset):
-    def __init__(self, IMG_DIR, file_list, is_test=False, transform=None):
+    def __init__(self, image_data, is_test=False, transform=None):
         self.is_test = is_test
-        self.root_path = IMG_DIR
-        self.file_list = file_list
+        self.image_data = image_data
         self.transform = transform
 
     def __len__(self):
-        return len(self.file_list)
+        return len(self.image_data)
 
     def __getitem__(self, index):
-        if index not in range(0, len(self.file_list)):
+        if index not in range(0, len(self.image_data)):
             return self.__getitem__(np.random.randint(0, self.__len__()))
-
-        file_id = self.file_list[index]
-        image_path = os.path.join(self.root_path, file_id[0])
-        image = load_image(image_path)
+        image = self.image_data[index][0]
         if self.transform:
             image = self.transform(image)
         if self.is_test is True:
             return image
         else:
-            return image, file_id[1], file_id[2], file_id[3]
+            return image, self.image_data[index][1:]
