e40ab984e90f39106e010457202814f77feb4ecf
mukesh-mehta <mukesh.iitr18@gmail.com>
2020-01-28 17:59:16 +0530

    trainer

--- src/train.py
+++ src/train.py
@@ -1,10 +1,11 @@
 import torch
 from torch import nn
+import pandas as pd
 import numpy as np
 
 
-def train(train, val, test, model_name, num_classes, model_out_path, device, epochs=10):
-
+def train(image_file_path, train_csv, num_classes, model_name, model_out_path, device, epochs=10):
+    dataframe = pd.read_csv(train_csv)
 
 
 
