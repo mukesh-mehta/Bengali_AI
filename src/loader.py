0bf3a2b7701f200a97b6845ccf7994e12ca07b3a
mukesh-mehta <mukesh.iitr18@gmail.com>
2020-01-29 14:00:56 +0530

    updated loader for space efficiency

--- src/loader.py
+++ src/loader.py
@@ -1,23 +1,29 @@
 import numpy as np
+import pandas as pd
 from torch.utils import data
+from torchvision import transforms
 
 
-class DataLoader(data.Dataset):
-    def __init__(self, image_data, is_test=False, transform=None):
+class ImageLoader(data.Dataset):
+    def __init__(self, labels_file, image_files, is_test=False, transform=transforms.Compose([transforms.ToTensor()])):
         self.is_test = is_test
-        self.image_data = image_data
         self.transform = transform
+        if not self.is_test:
+            self.labels_df = pd.read_csv(labels_file)[['grapheme_root', 'vowel_diacritic', 'consonant_diacritic']].values
+        self.image_df = pd.concat([pd.read_parquet(parque_file) for parque_file in image_files])
 
     def __len__(self):
-        return len(self.image_data)
+        return self.image_df.shape[0]
 
     def __getitem__(self, index):
-        if index not in range(0, len(self.image_data)):
+        if index not in range(0, self.image_df.shape[0]):
             return self.__getitem__(np.random.randint(0, self.__len__()))
-        image = self.image_data[index][0]
+
+        image = self.image_df.iloc[index].values[1:].reshape(-1, 137, 236).astype(int)
         if self.transform:
             image = self.transform(image)
         if self.is_test is True:
             return image
         else:
-            return image, self.image_data[index][1:]
+            labels = self.labels_df[index]
+            return image, labels
--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 import pandas as pd
 import numpy as np
+from loader import ImageLoader
 
 
 def train(parque_file_path,
@@ -10,17 +11,13 @@ def train(parque_file_path,
           model_name,
           model_out_path,
           device,
+          batch_size=16,
           epochs=10):
-    dataframe = pd.read_csv(train_csv)
-    parque_file = pd.concat([pd.read_parquet(parque_file) for parque_file in parque_file_path])
-    merged_dataframe = dataframe.merge(parque_file, left_on='image_id', right_on='image_id')
-    image_data_list = [(img, grapheme_root, vowel_diacritic, consonant_diacritic)
-                       for grapheme_root, vowel_diacritic, consonant_diacritic, img in
-                       zip(merged_dataframe['grapheme_root'],
-                            merged_dataframe['vowel_diacritic'],
-                            merged_dataframe['consonant_diacritic'],
-                            merged_dataframe.iloc[:,5:].values.reshape(-1, 137, 236))]
-    print(len(image_data_list))
+    loader = torch.utils.data.DataLoader(ImageLoader(train_csv, parque_file_path), batch_size=batch_size, shuffle=True, num_workers=8)
+    for img, labels in loader:
+        print(labels)
+        break
+
 
 
 # def evaluate(loader, model, loss_func, device, checkpoint=None, weights=None): grapheme_root    vowel_diacritic consonant_diacritic
