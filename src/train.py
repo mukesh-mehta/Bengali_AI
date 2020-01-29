f20ad38b6d29c0b3e55e41ffc6b6523d24fac60c
mukesh-mehta <mukesh.iitr18@gmail.com>
2020-01-29 23:23:06 +0530

    updated codes

--- src/train.py
+++ src/train.py
@@ -12,29 +12,31 @@ from torch.utils.tensorboard import SummaryWriter
 
 def train(parque_file_path,
           train_csv,
-          num_classes,
           model_name,
           model_out_path,
           device,
-          batch_size=32,
+          batch_size=128,
           epochs=10):
     writer = SummaryWriter('../runs')
     n_grapheme = 168
     n_vowel = 11
     n_consonant = 7
-    loader = torch.utils.data.DataLoader(ImageLoader(train_csv, parque_file_path), batch_size=batch_size, shuffle=True, num_workers=12)
+    loader = torch.utils.data.DataLoader(ImageLoader(train_csv, parque_file_path), batch_size=batch_size, shuffle=True, num_workers=8)
     model = initialize_model(model_name, n_grapheme+n_vowel+n_consonant, True).to(device)
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001,momentum=0.9)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     criterion =  nn.CrossEntropyLoss()
     global_step = 0
+    model.train()
     for epoch in range(epochs):
         loss=0
-        for i, (img, labels) in tqdm(enumerate(loader)):
+        for img, labels in tqdm(loader):
             img = img.type(torch.FloatTensor).permute(0, 2, 3, 1).to(device)
-            y_pred = model(img)
-            loss_grapheme = criterion(y_pred[:, :n_grapheme], labels[:,0])
-            loss_vowel = criterion(y_pred[:, n_grapheme:n_grapheme+n_vowel], labels[:,1])
-            loss_consonant = criterion(y_pred[:, n_grapheme+n_vowel:n_grapheme+n_vowel+n_consonant], labels[:,2])
+            labels = labels.type(torch.LongTensor).to(device)
+            pred = model(img)
+            preds = torch.split(pred, [n_grapheme, n_vowel, n_consonant], dim=1)
+            loss_grapheme = criterion(preds[0], labels[:,0])
+            loss_vowel = criterion(preds[1], labels[:,1])
+            loss_consonant = criterion(preds[2], labels[:,2])
             total_loss = loss_grapheme+loss_vowel+loss_consonant
             total_loss.backward()
             optimizer.step()
@@ -51,4 +53,4 @@ def train(parque_file_path,
 
 # def evaluate(loader, model, loss_func, device, checkpoint=None, weights=None): grapheme_root    vowel_diacritic consonant_diacritic
 train(["../data/bengaliai-cv19/train_image_data_{}.parquet".format(i) for i in range(4)],
-      "../data/bengaliai-cv19/train.csv",10,"resnet18", '../', 'cpu')
+      "../data/bengaliai-cv19/train.csv","resnext50_32x4d", '../', 'cuda')
