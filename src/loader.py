18fc31f78aadf077c8be61e9a8f5aed7ee7844af
mukesh-mehta <mukesh.iitr18@gmail.com>
2020-01-31 15:03:34 +0530

    added class weights

--- src/loader.py
+++ src/loader.py
@@ -1,16 +1,15 @@
 import numpy as np
-import pandas as pd
 from torch.utils import data
 from torchvision import transforms
 
 
 class ImageLoader(data.Dataset):
-    def __init__(self, labels_file, image_files, is_test=False, transform=transforms.Compose([transforms.ToTensor()])):
+    def __init__(self, labels_df, image_df, is_test=False, transform=transforms.Compose([transforms.ToTensor()])):
         self.is_test = is_test
         self.transform = transform
         if not self.is_test:
-            self.labels_df = pd.read_csv(labels_file)[['grapheme_root', 'vowel_diacritic', 'consonant_diacritic']].values
-        self.image_df = pd.concat([pd.read_parquet(parque_file) for parque_file in image_files])
+            self.labels_df = labels_df.values
+        self.image_df = image_df
 
     def __len__(self):
         return self.image_df.shape[0]
--- src/train.py
+++ src/train.py
@@ -1,27 +1,36 @@
 import torch
 import time
+import pandas as pd
 from torch import nn
 from tqdm import tqdm
 from loader import ImageLoader
 from model import initialize_model
 from torch.utils.tensorboard import SummaryWriter
+from sklearn.utils.class_weight import compute_class_weight
 
+n_grapheme = 168
+n_vowel = 11
+n_consonant = 7
 
 def train(parque_file_path,
           train_csv,
           model_name,
           model_out_path,
           device,
-          batch_size=256,
+          batch_size=128,
           epochs=10):
-    writer = SummaryWriter('/media/mukesh/36AD331451677000/bengali_ai/runs/{}'.format(int(time.time())))
-    n_grapheme = 168
-    n_vowel = 11
-    n_consonant = 7
-    loader = torch.utils.data.DataLoader(ImageLoader(train_csv, parque_file_path), batch_size=batch_size, shuffle=True, num_workers=8)
+    writer = SummaryWriter('../runs')#'/media/mukesh/36AD331451677000/bengali_ai/runs/{}'.format(int(time.time())))
+    labels_df = pd.read_csv(train_csv)[['grapheme_root', 'vowel_diacritic', 'consonant_diacritic']]
+    image_df = pd.concat([pd.read_parquet(parque_file) for parque_file in parque_file_path])
+    loader = torch.utils.data.DataLoader(ImageLoader(labels_df, image_df), batch_size=batch_size, shuffle=True, num_workers=8)
     model = initialize_model(model_name, n_grapheme+n_vowel+n_consonant, True).to(device)
+
+    vowel_weight = torch.FloatTensor(compute_class_weight('balanced', list(set(labels_df['vowel_diacritic'].values)), labels_df['vowel_diacritic'].values)).to(device)
+    grapheme_weight = torch.FloatTensor(compute_class_weight('balanced', list(set(labels_df['grapheme_root'].values)), labels_df['grapheme_root'].values)).to(device)
+    consonant_weight = torch.FloatTensor(compute_class_weight('balanced', list(set(labels_df['consonant_diacritic'].values)), labels_df['consonant_diacritic'].values)).to(device)
+
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    criterion =  nn.CrossEntropyLoss()
+    # criterion =  nn.CrossEntropyLoss()
     global_step = 0
     model.train()
     for epoch in range(epochs):
@@ -32,9 +41,9 @@ def train(parque_file_path,
             labels = labels.type(torch.LongTensor).to(device)
             pred = model(img)
             preds = torch.split(pred, [n_grapheme, n_vowel, n_consonant], dim=1)
-            loss_grapheme = criterion(preds[0], labels[:,0])
-            loss_vowel = criterion(preds[1], labels[:,1])
-            loss_consonant = criterion(preds[2], labels[:,2])
+            loss_grapheme = nn.CrossEntropyLoss(weight=grapheme_weight)(preds[0], labels[:,0])
+            loss_vowel = nn.CrossEntropyLoss(weight=vowel_weight)(preds[1], labels[:,1])
+            loss_consonant = nn.CrossEntropyLoss(weight=consonant_weight)(preds[2], labels[:,2])
             total_loss = loss_grapheme+loss_vowel+loss_consonant
             total_loss.backward(retain_graph=True)
             optimizer.step()
@@ -59,4 +68,4 @@ def train(parque_file_path,
 
 # def evaluate(loader, model, loss_func, device, checkpoint=None, weights=None): grapheme_root    vowel_diacritic consonant_diacritic
 train(["../data/bengaliai-cv19/train_image_data_{}.parquet".format(i) for i in range(4)],
-      "../data/bengaliai-cv19/train.csv","resnext50_32x4d", '../', 'cuda')
+      "../data/bengaliai-cv19/train.csv","resnet18", '../', 'cpu')
