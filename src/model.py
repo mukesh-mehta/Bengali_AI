96f5a82baba142de95e48b4d12eed229af524c5e
mukesh-mehta <mukesh.iitr18@gmail.com>
2020-02-06 12:23:27 +0530

    restructing codes

--- src/model.py
+++ src/model.py
@@ -1,46 +1,7 @@
 from torchvision import models
 from torch import nn
 import torch
-
-# implement mish activation function
-def f_mish(input):
-    '''
-    Applies the mish function element-wise:
-    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
-    '''
-    return input * torch.tanh(F.softplus(input))
-
-# implement class wrapper for mish activation function
-class mish(nn.Module):
-    '''
-    Applies the mish function element-wise:
-    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
-    '''
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, input):
-        return f_mish(input)
-
-
-def f_swish(input):
-    '''
-    Applies the swish function element-wise:
-    swish(x) = x * sigmoid(x)
-    '''
-    return input * torch.sigmoid(input)
-
-# implement class wrapper for swish activation function
-class swish(nn.Module):
-    '''
-    Applies the swish function element-wise:
-    swish(x) = x * sigmoid(x)
-    '''
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, input):
-        return f_swish(input)
+from utils import swish, mish
 
 
 class LinearBottleNeck(nn.Module):
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,45 @@
-from PIL import Image
-
+import torch
 
 def log_writer(writer, logs, step):
     for key, val in logs.items():
         writer.add_scalar(key, val, step)
     return writer
+
+def f_mish(input):
+    '''
+    Applies the mish function element-wise:
+    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+    '''
+    return input * torch.tanh(F.softplus(input))
+
+# implement class wrapper for mish activation function
+class mish(nn.Module):
+    '''
+    Applies the mish function element-wise:
+    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+    '''
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return f_mish(input)
+
+
+def f_swish(input):
+    '''
+    Applies the swish function element-wise:
+    swish(x) = x * sigmoid(x)
+    '''
+    return input * torch.sigmoid(input)
+
+# implement class wrapper for swish activation function
+class swish(nn.Module):
+    '''
+    Applies the swish function element-wise:
+    swish(x) = x * sigmoid(x)
+    '''
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return f_swish(input)
