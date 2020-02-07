c2ced5b1598211d04d23ad2cb44cf28b639d768a
mukesh-mehta <mukesh.iitr18@gmail.com>
2020-02-07 10:46:39 +0530

    model

--- src/model.py
+++ src/model.py
@@ -109,3 +109,25 @@ class MobileNetV2(nn.Module):
 
 def mobilenetv2(activation = 'relu'):
     return MobileNetV2(class_num = 1000, activation = activation)
+
+
+class BengaliModel(nn.Module):
+    def __init__(self, backbone_model):
+        super(BengaliModel, self).__init__()
+        #self.conv = nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3)
+        self.backbone_model = backbone_model
+        self.fc1 = nn.Linear(in_features=1000, out_features=168) # grapheme_root
+        self.fc2 = nn.Linear(in_features=1000, out_features=11) # vowel_diacritic
+        self.fc3 = nn.Linear(in_features=1000, out_features=7) # consonant_diacritic
+        
+    def forward(self, x):
+        # pass through the backbone model
+        #y = self.conv(x)
+        y = self.backbone_model(x)
+        
+        # multi-output
+        grapheme_root = self.fc1(y)
+        vowel_diacritic = self.fc2(y)
+        consonant_diacritic = self.fc3(y)
+        
+        return grapheme_root, vowel_diacritic, consonant_diacritic
