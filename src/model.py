ec372c71e1a886e6c3bca9377a698396b8e172f7
mukesh-mehta <mukesh.iitr18@gmail.com>
2020-02-10 11:38:05 +0530

    removed SEBasicblock

--- src/model.py
+++ src/model.py
@@ -37,61 +37,6 @@ def conv1x1(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 
-class SEBasicBlock(nn.Module):
-    expansion = 1
-    __constants__ = ['downsample']
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None):
-        super(SEBasicBlock, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        if groups != 1 or base_width != 64:
-            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
-        if dilation > 1:
-            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = norm_layer(planes)
-        # SE
-        self.global_pool = nn.AdaptiveAvgPool2d(1)
-        self.conv_down = nn.Conv2d(
-            planes * 4, planes // 4, kernel_size=1, bias=False)
-        self.conv_up = nn.Conv2d(
-            planes // 4, planes * 4, kernel_size=1, bias=False)
-        self.sig = nn.Sigmoid()
-        #
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        
-        SE = self.global_pool(out)
-        SE = self.conv_down(SE)
-        SE = self.relu(SE)
-        SE = self.conv_up(SE)
-        SE = self.sig(SE)
-        
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out = SE*out + identity
-        out = self.relu(out)
-
-        return out
-
-
 class SEBottleneck(nn.Module):
     expansion = 4
     __constants__ = ['downsample']
